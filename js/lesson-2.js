79758324dd29a45eafc3856740ce008a5d31d0a1
Denys <denis.itsme.1@gmail.com>
2024-04-28 10:47:40 +0300

    task-1

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -1,12 +1,38 @@
 'use strict';
-const categoriesItems = document
-  .querySelector('#categories')
-  .querySelectorAll('.item');
-console.log(`Number of categories: ${categoriesItems.length}`);
+// Створіть масив styles з елементами 'Джаз' і 'Блюз'
+//Додайте в кінець 'Рок-н-ролл'
+//Заменіть значення 'Блюз' на 'Класика'
+// Напишіть функцію logItems (array), яка приймає
+// масив і використовує цикл for, який для кожного елемента
+//буде виводити повідомлення у форматі:
+//<номер елемента> - <значення елемента>
+//Нумерація має починатись з 1
 
-categoriesItems.forEach(item => {
-  const categoryTitle = item.querySelector('h2').textContent;
-  console.log(`Category: ${categoryTitle}`);
-  const categoryElementQuantity = item.querySelectorAll('ul li').length;
-  console.log(`Elements: ${categoryElementQuantity}`);
-});
+const styles = ['Jazz', 'Blues'];
+styles.push('Rock-and-Roll');
+// styles[1] = 'Classic';
+const indexOfBlues = styles.indexOf('Blues');
+if (indexOfBlues !== -1) {
+  styles[indexOfBlues] = 'Classic';
+}
+
+function logItems(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(`${i + 1} - ${array[i]}`);
+  }
+}
+
+logItems(styles);
+//===================================
+// напиши функцію яка сумуватиме сусідні числа і пушити в новий масив
+const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+function sumSibling(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length - 1; i++) {
+    const currentValue = array[i] + array[i + 1];
+    newArray.push(currentValue);
+  }
+  return newArray;
+}
+console.log(sumSibling(someArr));
